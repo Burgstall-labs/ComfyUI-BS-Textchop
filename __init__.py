022443e975506d07655d3958a664af451216afc4
Burgstall-labs <kauko.book@gmail.com>
2025-04-05 10:34:06 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,28 +1,8 @@
-# --- START OF FILE __init__.py ---
+from .bs_textchop import TextExtractBetweenSmart # Import the new class name
 
-# Import the node class
-from .bs_textchop import TextExtractBetween
-
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "BSTextChop": TextExtractBetween
+    "BSTextChop": TextExtractBetweenSmart, # Update class name here
 }
-
-# A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "BSTextChop": "BS Textchop"
+    "BSTextChop": "BS Textchop (Smart)", # Update display name
 }
-
-# Optional: Add a version string
-__version__ = '1.0.0'
-
-# Optional: A web directory mapping if the node needs to serve web files
-# WEB_DIRECTORY = "./js" # Example if you had javascript files
-
-print("------------------------------------------")
-print("ComfyUI-BS-Textchop: Loaded")
-print("------------------------------------------")
-
-
-# --- END OF FILE __init__.py ---
